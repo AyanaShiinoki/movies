277c2c0d2e636282008dcf76c1de6c0f110f0617
MacUser <macuser@MacUsernoMacBook-Pro.local>
2019-12-11 14:57:51 +0900

    [add]movie model&controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_11_053344) do
+ActiveRecord::Schema.define(version: 2019_12_11_055318) do
+
+  create_table "movies", force: :cascade do |t|
+    t.text "movie_title"
+    t.text "caption"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
