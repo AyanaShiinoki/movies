6adc0351395c8496d772fd0f78460095bd65a511
MacUser <macuser@MacUsernoMacBook-Pro.local>
2019-12-11 15:42:28 +0900

    [add]users controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+	skip.before_action :authenticate_user!
   def top
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/index'
   root 'home/top'
-  get 'home/about'
+  get '/about' => 'home#about'
   devise_for :users
   resources :movies, only: [:new, :create, :index, :show]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
