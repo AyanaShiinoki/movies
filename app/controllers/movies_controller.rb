e080d265a8036998b4b271d2b229bcb27bfe095f
MacUser <macuser@MacUsernoMacBook-Pro.local>
2019-12-11 15:29:16 +0900

    [add]home controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,28 @@
 class MoviesController < ApplicationController
+
+	def new
+	 @movie = Movie.new
+	end
+
+	def create
+	 @movie = Movie.new
+	 @movie.user_id = current_user.id
+	 @movie.save
+	 redirect_to movies_path
+	end
+
+	def index
+	 @movies = Movie.all
+	end
+
+	def show
+	 @movie = Movie.find(params[:id])
+	end
+
+
+private
+	def movie_params
+		params.require(:movie).permit(:movie_title,:caption)
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'home/top'
+  get 'home/about'
   devise_for :users
+  resources :movies, only: [:new, :create, :index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
